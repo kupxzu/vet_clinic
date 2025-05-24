37b34184dfcf7ea181f8167a66e10aa048bb3a14
chicken.penguin <carlcastuel@gmail.com>
2025-05-24 11:54:35 +0800

    updates

--- app/config/Connection.php
+++ app/config/Connection.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost'); 
 define('DB_NAME', 'vet_db');   
 define('DB_USER', 'root');     
-define('DB_PASS', 'root');         
+define('DB_PASS', 'root');  // remove this root password if you are using XAMPP       
 
 try {
     // Create a new PDO instance
