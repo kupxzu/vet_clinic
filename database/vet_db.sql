db2e3b543ba1c413fd0b2af7884e2f8b8982deef
chicken.penguin <carlcastuel@gmail.com>
2025-05-24 11:53:14 +0800

    new db

--- database/vet_db.sql
+++ database/vet_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 23, 2025 at 09:53 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: May 24, 2025 at 11:51 AM
+-- Server version: 5.7.24
+-- PHP Version: 8.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,17 +35,19 @@ CREATE TABLE `appointments` (
   `appointment_time` time NOT NULL,
   `appointment_type` varchar(100) NOT NULL DEFAULT 'Check-up',
   `status` enum('pending','confirmed','completed','cancelled') NOT NULL DEFAULT 'pending',
-  `notes` text DEFAULT NULL,
-  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `date_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `notes` text,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `appointments`
 --
 
 INSERT INTO `appointments` (`id`, `client_id`, `pet_id`, `appointment_date`, `appointment_time`, `appointment_type`, `status`, `notes`, `date_created`, `date_updated`) VALUES
-(1, 2, 1, '2025-05-24', '03:26:00', 'Check-up', 'pending', 'Check ups', '2025-05-23 19:26:28', '2025-05-23 19:36:51');
+(5, 2, 1, '2025-05-24', '09:32:00', 'Check-up', 'completed', '[Visit Recorded] diagnosed ', '2025-05-24 01:32:42', '2025-05-24 01:49:06'),
+(6, 3, 2, '2025-05-24', '09:53:00', 'Check-up', 'completed', '[Visit Recorded] qwe', '2025-05-24 01:53:54', '2025-05-24 01:54:19'),
+(7, 3, 2, '2025-05-24', '09:54:00', 'Follow-up', 'completed', '[Visit Recorded] cured', '2025-05-24 01:54:19', '2025-05-24 03:10:31');
 
 -- --------------------------------------------------------
 
@@ -57,19 +59,22 @@ CREATE TABLE `pets` (
   `id` int(11) NOT NULL,
   `client_id` int(11) NOT NULL,
   `pet_name` varchar(255) NOT NULL,
+  `pet_sex` enum('female','male') NOT NULL,
+  `pet_species` enum('dog','cat','others') NOT NULL,
   `breed_id` int(11) NOT NULL,
   `pet_age` int(11) DEFAULT NULL,
-  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `date_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `pets`
 --
 
-INSERT INTO `pets` (`id`, `client_id`, `pet_name`, `breed_id`, `pet_age`, `date_created`, `date_updated`) VALUES
-(1, 2, 'Buddy', 1, 2, '2025-05-23 19:05:57', '2025-05-23 19:05:57'),
-(2, 3, 'Bitchass', 2, 1, '2025-05-23 19:52:12', '2025-05-23 19:52:12');
+INSERT INTO `pets` (`id`, `client_id`, `pet_name`, `pet_sex`, `pet_species`, `breed_id`, `pet_age`, `date_created`, `date_updated`) VALUES
+(1, 2, 'Buddy', 'male', 'dog', 1, 1, '2025-05-23 19:05:57', '2025-05-24 01:53:29'),
+(2, 3, 'Bitchass', 'male', 'dog', 1, 1, '2025-05-23 19:52:12', '2025-05-24 01:53:24'),
+(3, 3, 'Diddy', 'male', 'dog', 1, 1, '2025-05-24 01:02:29', '2025-05-24 01:53:23');
 
 -- --------------------------------------------------------
 
@@ -80,8 +85,8 @@ INSERT INTO `pets` (`id`, `client_id`, `pet_name`, `breed_id`, `pet_age`, `date_
 CREATE TABLE `pet_breeds` (
   `id` int(11) NOT NULL,
   `breed_name` varchar(100) NOT NULL,
-  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `pet_breeds`
@@ -90,6 +95,28 @@ CREATE TABLE `pet_breeds` (
 INSERT INTO `pet_breeds` (`id`, `breed_name`, `date_created`) VALUES
 (1, 'Golden Retriver', '2025-05-23 19:05:19'),
 (2, 'Sphinx', '2025-05-23 19:51:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
+  `service_name` varchar(255) NOT NULL,
+  `service_fee` decimal(10,2) NOT NULL,
+  `description` text,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `services`
+--
+
+INSERT INTO `services` (`id`, `service_name`, `service_fee`, `description`, `date_created`, `date_updated`) VALUES
+(1, 'Check Up', '900.00', 'Animal Check-ups', '2025-05-24 00:53:12', '2025-05-24 00:53:12');
 
 -- --------------------------------------------------------
 
@@ -104,9 +131,9 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` enum('admin','client') NOT NULL DEFAULT 'client',
-  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `date_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
@@ -117,6 +144,35 @@ INSERT INTO `users` (`id`, `fullname`, `email`, `username`, `password`, `role`, 
 (2, 'Client 1', 'client@example.com', 'Client 1', '$2y$10$na9dYYzH9INDElzat4WmC.PhNQNPwDuAcZ8vvJWU2scv8IJj39D0C', 'client', '2025-05-23 18:35:33', '2025-05-23 18:35:33'),
 (3, 'Client 2', 'client2@example.com', 'Client 2', '$2y$10$/vgv54EZ.CJaxRTs5t4Ld.3uoO7wNnG1Suhn3.ibY1i2wnFkCR2rq', 'client', '2025-05-23 18:44:26', '2025-05-23 18:44:26'),
 (4, 'Client 3', 'client3@example.com', 'Client 3', '$2y$10$RoFgklfik2zAc7FVyEkeUu5fIKw1cRD79MILevU0Mo8DDdpJiUReO', 'client', '2025-05-23 18:45:29', '2025-05-23 18:45:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visit_records`
+--
+
+CREATE TABLE `visit_records` (
+  `id` int(11) NOT NULL,
+  `appointment_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `pet_id` int(11) NOT NULL,
+  `service_id` int(11) DEFAULT NULL,
+  `visit_notes` text,
+  `additional_fees` decimal(10,2) DEFAULT '0.00',
+  `total_amount` decimal(10,2) DEFAULT NULL,
+  `discharged` enum('yes','no') NOT NULL DEFAULT 'yes',
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `visit_records`
+--
+
+INSERT INTO `visit_records` (`id`, `appointment_id`, `client_id`, `pet_id`, `service_id`, `visit_notes`, `additional_fees`, `total_amount`, `discharged`, `date_created`, `date_updated`) VALUES
+(10, 5, 2, 1, 1, 'diagnosed ', '0.00', '900.00', 'no', '2025-05-24 01:49:06', '2025-05-24 01:49:06'),
+(11, 6, 3, 2, 1, 'qwe', '0.00', '900.00', 'no', '2025-05-24 01:54:19', '2025-05-24 01:54:19'),
+(12, 7, 3, 2, 1, 'cured', '0.00', '900.00', 'yes', '2025-05-24 03:10:31', '2025-05-24 03:10:31');
 
 --
 -- Indexes for dumped tables
@@ -146,12 +202,29 @@ ALTER TABLE `pet_breeds`
   ADD UNIQUE KEY `breed_name` (`breed_name`);
 
 --
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `service_name` (`service_name`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`),
   ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `visit_records`
+--
+ALTER TABLE `visit_records`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `appointment_id` (`appointment_id`),
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `pet_id` (`pet_id`),
+  ADD KEY `service_id` (`service_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -161,13 +234,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `pets`
 --
 ALTER TABLE `pets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `pet_breeds`
@@ -176,10 +249,22 @@ ALTER TABLE `pet_breeds`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `visit_records`
+--
+ALTER TABLE `visit_records`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
@@ -198,6 +283,15 @@ ALTER TABLE `appointments`
 ALTER TABLE `pets`
   ADD CONSTRAINT `pets_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `pets_ibfk_2` FOREIGN KEY (`breed_id`) REFERENCES `pet_breeds` (`id`);
+
+--
+-- Constraints for table `visit_records`
+--
+ALTER TABLE `visit_records`
+  ADD CONSTRAINT `visit_records_ibfk_1` FOREIGN KEY (`appointment_id`) REFERENCES `appointments` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `visit_records_ibfk_2` FOREIGN KEY (`client_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `visit_records_ibfk_3` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `visit_records_ibfk_4` FOREIGN KEY (`service_id`) REFERENCES `services` (`id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
